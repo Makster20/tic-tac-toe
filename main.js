e618d9be2f878b8152abbd8f2e20cc6f15d1bef5
Makster20 <mikhaelsaidshah123@gmail.com>
2025-01-08 17:26:50 +0400

    add player functionality

--- main.js
+++ main.js
@@ -7,8 +7,6 @@ function Gameboard(){
 
     const getBoard = () => gameboard;
 
-    const cells = document.querySelectorAll('.grid div')
-    console.log(cells);
     
 
     return {
@@ -18,18 +16,75 @@ function Gameboard(){
 
 
 function Player(){
+
+    // DOM elements
+    const cover = document.querySelector('.cover');
     const againstChoices = document.querySelectorAll('.choices div');
     const againstPlayer2 = againstChoices[0];
     const againstComputer = againstChoices[1];
+    const playButton = document.querySelector('.cover button');
 
-    againstChoices.forEach((choice) => {
-        choice.addEventListener('click', () => {
-            
+    // Normal variables
+    let opponent;
+
+    let chooseOpponent = () => {
+        // Select opponent
+        againstPlayer2.addEventListener('click', () => {
+            againstPlayer2.style.backgroundColor = 'darkblue';
+            againstPlayer2.style.color = 'white';
+            againstPlayer2.classList.add('clicked');
+
+            if(againstPlayer2.style.backgroundColor == 'darkblue' && againstComputer.style.color == 'white'){
+                againstComputer.style.backgroundColor = 'white';
+                againstComputer.style.color = 'darkblue';
+
+                againstComputer.classList.remove('clicked');
+            }
+        })
+
+        againstComputer.addEventListener('click', () => {
+            againstComputer.style.backgroundColor = 'darkblue';
+            againstComputer.style.color = 'white';
+            againstComputer.classList.add('clicked');
+
+            if(againstComputer.style.backgroundColor == 'darkblue' && againstComputer.style.color == 'white'){
+                againstPlayer2.style.backgroundColor = 'white';
+                againstPlayer2.style.color = 'darkblue';
+
+                againstPlayer2.classList.remove('clicked');
+            }
+        })
+
+        playButton.addEventListener('click', () => {
+            againstChoices.forEach(choice => {
+                if(choice.classList.contains('clicked')){
+                    opponent = choice.classList[0];
+                }
+            });
+            cover.style.display = 'none';
         });
-    });
+    }
+
+    let getOpponent = () => opponent;
+
+    return {
+        chooseOpponent,
+        getOpponent,
+    }
 }
 
 
 function GameController(){
 
 }
+
+
+function ScreenController(){
+    let player = Player();
+    player.chooseOpponent();
+    opponent = player.getOpponent();
+    
+
+}
+
+ScreenController()
